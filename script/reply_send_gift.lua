4b7a2d6afa825734295df328d93c3651d09f71cf
string.Empty <mystring.empty@gmail.com>
2022-11-10 20:06:34 +0800

    update reply_send_gift

--- script/reply_send_gift.lua
+++ script/reply_send_gift.lua
@@ -1,8 +1,7 @@
-rcv_gift_order = "送"..selfName
-daily_gift_limit = 3   --单日次数上限
-gift_favor_limit = 100 --送礼能达到的好感上限
-
-gift_list = {   --特殊对待的礼物名
+local gifts = getSelfData("gifts.json")
+local daily_gift_limit = gifts.daili_times or 3   --单日次数上限
+local gift_favor_limit = gifts.favor_limit or 100 --送礼能达到的好感上限
+local gift_list = gifts.special or {   --特殊对待的礼物名
     ["肖战"] = {
         refuse = true, --是否拒收
         favor = -10,  --好感变化
@@ -14,18 +13,18 @@ gift_list = {   --特殊对待的礼物名
         reply = "嘿嘿，谢谢{nick}送的陪睡券～晚上这就去找丛雨~",
     },
 }
-
-local today_gift = getUserToday(msg.fromQQ,"rcv_gifts")
+local user_today = getUserToday(msg.uid)
+local today_gift = user_today.rcv_gifts or 0
 if(today_gift >= daily_gift_limit)then
     return "够啦～{nick}今天已经送得太多了"
 end
-setUserToday(msg.fromQQ, "rcv_gifts", today_gift+1)
-local gift = string.match(msg.fromMsg,"^[%s]*(.-)[%s]*$",#rcv_gift_order+1)
-if(gift=="")then
+user_today.rcv_gifts = today_gift+1
+local gift = msg.suffix
+if gift=="" then
     return "嗯？{nick}要送{self}什么？"
 end
-local me = getDiceQQ()
-local self_today_gift = getUserToday(me, "gifts")
+local my_today = getUserToday(getDiceQQ())
+local self_today_gift = my_today.gifts or 0
 local favor = getUserConf(msg.fromQQ, favor_title, 0)
 local react = gift_list[gift]
 if(react)then
@@ -38,11 +37,11 @@ if(react)then
     if(react.refuse)then
         return react.reply
     end
-    setUserToday(me, "gifts", self_today_gift+1)
+    my_today.gifts = self_today_gift+1
     return react.reply.."\n今日收礼:"..string.format("%d",self_today_gift).."件"
 end
 --local self_total_gift = getUserConf(me, "gifts", 0)+1
 --setUserConf(me, "gifts", self_total_gift)
 if(ranint(1,gift_favor_limit)>favor)then setUserConf(msg.fromQQ, favor_title, favor + 1) end
-setUserToday(me, "gifts", self_today_gift+1)
+my_today.gifts = self_today_gift+1
 return "谢谢{nick}送的"..gift.."~\n今日收礼:"..string.format("%d",self_today_gift).."件"
